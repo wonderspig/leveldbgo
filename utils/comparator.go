aa044a521cf791063b20f47385c972c701d53832
tyf <448371352@qq.com>
2019-07-09 10:52:53 +0800

    格式整理

--- utils/comparator.go
+++ utils/comparator.go
@@ -1,18 +1,15 @@
 package utils
 
-
 //相当于一个接口
-type Comparator func(a,b interface{}) int
+type Comparator func(a, b interface{}) int
 
-
-
-func IntComparator(a,b interface{}) int {
-	firstNum:=a.(int)
-	secondNum:=b.(int)
+func IntComparator(a, b interface{}) int {
+	firstNum := a.(int)
+	secondNum := b.(int)
 	switch {
-	case firstNum>secondNum:
+	case firstNum > secondNum:
 		return 1
-	case firstNum<secondNum:
+	case firstNum < secondNum:
 		return -1
 	default:
 		break
